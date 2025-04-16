7701ccddf404c958ffc66e56cd1bf3ee3cf8656d
Bianca Monteiro Fernandes Corrêa <me@aquabianca.dev>
2025-04-15 21:12:55 -0300

    feat: runs selected command

--- miranda/cmd/main/main.go
+++ miranda/cmd/main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"os/exec"
 
 	"github.com/beecorrea/weaves/miranda/pkg/hackerrun.go"
 	tea "github.com/charmbracelet/bubbletea"
@@ -10,15 +11,25 @@ import (
 
 func main() {
 	p := tea.NewProgram(hackerrun.InitModel("miranda"))
-	hr, err := p.Run()
+	m, err := p.Run()
 	if err != nil {
 		panic(err)
 	}
 
-	selected, _ := hr.(hackerrun.Hackerrun)
-	if selected.Selected == nil {
+	hackRun := m.(hackerrun.Hackerrun)
+	selected := hackRun.Selected
+	if selected == nil {
 		log.Println("no hack selected")
 		os.Exit(0)
 	}
-	log.Println("Running hack:", selected.Selected.Name)
+	// TODO(Bia): Move hackerrun to another package.
+	log.Println("Running hack:", selected.Name)
+	c := exec.Command("/bin/sh", selected.Path)
+	c.Stdin = os.Stdin
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	if err := c.Run(); err != nil {
+		panic(err)
+	}
+	log.Println("Finished")
 }
